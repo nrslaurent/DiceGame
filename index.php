fef13c25679781d2f80e771c984426f9b773f56d
Laurent NARSIMOULOU <nrslaurent@icloud.com>
2021-06-20 08:46:36 +0200

    modify the title

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>Document</title>
+    <title>Dice Game</title>
     <link rel="preconnect" href="https://fonts.gstatic.com" />
     <link
       href="https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,100;1,100;1,400&display=swap"
